98661ae041efb9af25e7cdb10e64cd426daab9a0
wesbro <wrbrown12@gmail.com>
2022-01-28 19:12:07 -0500

    shuffle, flip, counter  and overlay are working

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ class Game {
         this.cardsArray = cards;
         this.totalTime = totalTime;
         this.timeRemaining = totalTime;
-        this.timer = document.getElementsById('time-remaining');
+        this.timer = document.getElementById('time-remaining');
         this.ticker = document.getElementById('clicks');
 
     }
@@ -33,14 +33,29 @@ class Game {
         this.timeRemaining = this.totalTime;
         this.matchedCards = [];
         this.busy = true;
-    }
+        this.shuffleCards();
+        }
     flipCard(card) {
         if (this.canFlip(card)) {
             this.totalClicks++;
             this.ticker.innerText = this.totalClicks;
+            card.classList.add('visable');
+            console.log('ah, you clicked me');
+
+            //if statement to check for a match
 
         }
     }
+// Card Shuffle- Fisher-Yates shuffle\\
+    shuffleCards() {
+        for (let i = this.cardsArray.length - 1; i > 0; i--) {
+            let randInd = Math.floor(Math.random() * (i + 1));
+            this.cardsArray[randInd].style.order = i;
+            this.cardsArray[i].style.order = randInd;
+        }
+    }
+
+
     canFlip(card) {
         return true;
         // return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
@@ -48,20 +63,39 @@ class Game {
 
 }
 
+// function play() {
+//     let cards = Array.from(document.getElementsByClassName('card'));
+//     let game = new Game(100, cards);
+//     game.startGame();
+
+//     cards.forEach(card => {
+//         card.addEventListener('click', (evt) =>
+//             game.flipCard(card))
+//         console.log('flippin cards')
+//     })
+// }
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new Game(100, cards)
+    let game = new Game(100, cards);
 
     overlays.forEach(overlay => {
-        overlay.addEventListener('click', (evt) => {
-            overlay.classList.remove('visable');
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove("visable");
             game.startGame();
         });
     });
     cards.forEach(card => {
-        card.addEventListener('click', (evt) => {
+        card.addEventListener('click', () => {
             game.flipCard(card);
         });
     });
 }
+
